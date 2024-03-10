aacf45f9673fd2feaf3185b3be09eef7278d7f4d
gramizor <granin17@yandex.ru>
2024-03-10 20:19:30 +0300

    hotfix

--- src/Chart/Chart.tsx
+++ src/Chart/Chart.tsx
@@ -9,43 +9,37 @@ interface ChartProps {
 
 const Chart: React.FC<ChartProps> = ({ jsonData }) => {
     const [selectedDateRange, setSelectedDateRange] = React.useState<[Date | null, Date | null]>([null, null]);
+    const [transformedData, setTransformedData] = React.useState<any[]>([]);
+
+    useEffect(() => {
+        transformData();
+    }, [selectedDateRange]);
 
     const transformData = () => {
-        const transformedData = [];
+        if (selectedDateRange[0] && selectedDateRange[1]) {
+            const filteredData = jsonData.filter(item => {
+                const fromDate = selectedDateRange[0] as Date;
+                const toDate = selectedDateRange[1] as Date;
+                const itemDate = new Date(item.from);
 
-        for (let i = 0; i < jsonData.length; i++) {
-            const currentItem = jsonData[i];
+                return itemDate >= fromDate && itemDate <= toDate;
+            });
 
-            if (currentItem) {
-                const fromTime = new Date(currentItem.from);
-                const toTime = new Date(currentItem.to);
-                // const segmentDuration = toTime.getTime() - fromTime.getTime();
-                const segmentSpeed = currentItem.speed;
+            const transformedData = filteredData.map(item => {
+                const fromTime = new Date(item.from).toLocaleString('en-GB').replace(',', '');
+                return { date: fromTime, speed: item.speed };
+            });
 
-                // Добавляем первую точку каждого отрезка
-                transformedData.push({ date: fromTime.toLocaleString(), speed: segmentSpeed });
+            setTransformedData(transformedData);
+        } else {
+            // Если диапазон дат не выбран, просто используем все данные
+            const allData = jsonData.map(item => {
+                const fromTime = new Date(item.from).toLocaleString('en-GB').replace(',', '');
+                return { date: fromTime, speed: item.speed };
+            });
 
-                // Добавляем вторую точку, если скорость не равна нулю и следующая скорость не равна нулю
-                if (segmentSpeed !== 0 && (i < jsonData.length - 1) && jsonData[i + 1].speed !== 0) {
-                    const secondActiveTime = new Date(toTime.getTime()).toLocaleString();
-                    transformedData.push({ date: secondActiveTime, speed: segmentSpeed });
-                }
-
-                // Добавляем точку, если скорость обнуляется
-                if (i < jsonData.length - 1) {
-                    const nextItem = jsonData[i + 1];
-                    const gapStart = new Date(toTime.getTime() + 1).toLocaleString();
-                    const gapEnd = new Date(nextItem.from).toLocaleString();
-                    transformedData.push({ date: gapStart, speed: 0 });
-                    transformedData.push({ date: gapEnd, speed: 0 });
-                } else {
-                    // Если это последний элемент в JSON, добавляем последнюю точку "to"
-                    transformedData.push({ date: toTime.toLocaleString(), speed: segmentSpeed });
-                }
-            }
+            setTransformedData(allData);
         }
-
-        return transformedData;
     };
     useEffect(() => console.log(selectedDateRange), [selectedDateRange]);
 
@@ -62,7 +56,7 @@ const Chart: React.FC<ChartProps> = ({ jsonData }) => {
             <div className='chart'>
                 <AreaChart
                     h={300}
-                    data={transformData()}
+                    data={transformedData}
                     dataKey="date"
                     series={[{ name: 'speed', color: 'indigo.6' }]}
                     curveType="linear"
@@ -73,5 +67,6 @@ const Chart: React.FC<ChartProps> = ({ jsonData }) => {
         </div>
     );
 };
+
 
 export default Chart;

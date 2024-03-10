7d43f6bc2367110c44cfef1fd60c7c3e39a0be0b
gramizor <granin17@yandex.ru>
2024-03-10 19:30:36 +0300

    hotfix

--- src/Chart/Chart.tsx
+++ src/Chart/Chart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { DatePicker } from '@mantine/dates';
 import { AreaChart } from '@mantine/charts';
 import './Chart.scss';
